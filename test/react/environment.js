1c0933295720d7171541a9be2bb120e56315f940
manuelfernando <manuelcamaron@hotmail.com>
2017-06-06 18:56:15 -0500

    add unit test to validate the integration with react

---
+++ test/react/environment.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { expect } from 'chai';
+import { mount } from 'enzyme';
+import { Mock1, Mock2 } from '../mocks';
+
+require('../../react-environment/dom-mock')('<html><body></body></html>');
+
+const mock1 = mount(<Mock1 keys={{subscribeKey: 'demo', publishKey: 'demo-36'}}/>);
+const mock2 = mount(<Mock2 keys={{subscribeKey: 'demo', publishKey: 'demo-36'}}/>);
+
+
+describe('#environment test', () => {
+  it('it is to able to make an instance', () => {
+    expect(mock1.pubnub).to.not.be.null;
+  });
+
+  it('it is to able to initialize states', () => {
+    expect(mock1.state()).to.not.be.null;
+    expect(mock1.state().pn_messages).to.be.an('object');
+    expect(mock1.state().pn_presence).to.be.an('object');
+    expect(mock1.state().pn_status).to.be.an('object');
+  });
+
+  it('it is to able to set states without affecting states already defined', () => {
+    expect(mock2.state()).to.not.be.null;
+    expect(mock2.state().name).to.be.equal('PubNub React SDK');
+    expect(mock2.state().pn_messages).to.be.an('object');
+    expect(mock2.state().pn_presence).to.be.an('object');
+    expect(mock2.state().pn_status).to.be.an('object');
+  });
+});
