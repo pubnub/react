54c9649d3c6c293b28b12c8794e1c20c33457069
manuelfernando <manuelcamaron@hotmail.com>
2017-06-06 18:49:17 -0500

    add triggers handler

---
+++ src/triggerEvents/trigger.js
@@ -0,0 +1,15 @@
+export class Trigger {
+  static isSubscribe(channel) {
+
+  }
+
+  static subscribeChannels({channels, triggerEvents}) {
+    channels.forEach((channel) => {
+
+    });
+  }
+
+  static unsubscribeChannels({channels}) {
+
+  }
+}
