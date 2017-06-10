c9295eaa999ae4797f08429c3e835786f256084d
manuelfernando <manuelcamaron@hotmail.com>
2017-06-09 19:05:22 -0500

    add unit tests to getStatus feature

---
+++ test/react/status.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { expect } from 'chai';
+import { mount } from 'enzyme';
+import { Mock2 } from '../mocks';
+
+require('../../react-environment/dom-mock')('<html><body></body></html>');
+
+const mock2 = mount(<Mock2 keys={ { subscribeKey: 'demo', publishKey: 'demo-36' } }/>);
+
+mock2.node.pubnub.init(mock2.node);
+
+
+describe('#status event', () => {
+  it('it is to able to invoke a render if the status change', (done) => {
+    expect(mock2.state().pn_status).to.be.an('object');
+    done();
+  });
+
+  it('it is to able to receive the status from PN network', (done) => {
+    mock2.node.pubnub.getStatus((st) => {
+      expect(st.category).to.be.equal('PNConnectedCategory');
+      done();
+    });
+
+    mock2.node.pubnub.subscribe({ channels: ['channel_1'] });
+  });
+
+  it('it is to able to update the state: pn_status', (done) => {
+    expect(mock2.state().pn_status.category).to.be.equal('PNConnectedCategory');
+    done();
+  });
+});
